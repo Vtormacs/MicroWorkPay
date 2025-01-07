d6005e1222bc516ac7f86dbfe3ded2b1ccbe520b
Vtormacs <macskids98play@hotmail.com>
2025-01-07 05:06:13 -0300

    feat: Criacao de workerController com endpoint de buscar todos os workers

--- hr-worket/src/main/java/com/MicroWorkPay/hr_worket/controllers/WorkerController.java
+++ hr-worket/src/main/java/com/MicroWorkPay/hr_worket/controllers/WorkerController.java
@@ -1,9 +1,28 @@
 package com.MicroWorkPay.hr_worket.controllers;
 
+import com.MicroWorkPay.hr_worket.DTOs.WorkerInfoDTO;
+import com.MicroWorkPay.hr_worket.services.WorkerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/workers")
 public class WorkerController {
+
+    @Autowired
+    private WorkerService workerService;
+
+    @GetMapping
+    public ResponseEntity<List<WorkerInfoDTO>> getAllWorkers() {
+        try {
+            return ResponseEntity.ok(workerService.findAll());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
