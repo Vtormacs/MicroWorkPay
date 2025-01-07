c74277afd04707831c921097bbd4de44b9b3566e
Vtormacs <macskids98play@hotmail.com>
2025-01-07 15:40:24 -0300

    feat: Criacao da entidade Payment

---
+++ hr-payroll/src/main/java/com/MicroWorkPay/hr_playroll/entities/Payment.java
@@ -0,0 +1,24 @@
+package com.MicroWorkPay.hr_playroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Payment implements Serializable {
+    static final long serialVersionUID = 1L;
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+    public double getTotal() {
+        return days * dailyIncome;
+    }
+}
