84163015a35f71c9bffe1e4a129c63e8d73725de
Vtormacs <macskids98play@hotmail.com>
2025-01-07 03:30:01 -0300

    feat(hr-worker): criação de projeto hr-worker

---
+++ hr-worket/src/main/java/com/MicroWorkPay/hr_worket/HrWorketApplication.java
@@ -0,0 +1,13 @@
+package com.MicroWorkPay.hr_worket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorketApplication.class, args);
+	}
+
+}
---
+++ hr-worket/src/test/java/com/MicroWorkPay/hr_worket/HrWorketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MicroWorkPay.hr_worket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
