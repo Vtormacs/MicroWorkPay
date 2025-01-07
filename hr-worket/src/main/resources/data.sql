042824c6a380ae2d187aa65a08e3fa1ad4097d1e
Vtormacs <macskids98play@hotmail.com>
2025-01-07 04:07:10 -0300

    feat: criação de data.sql para ter alguns trabalhadores de testes

---
+++ hr-worket/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (id, name, daily_Income) VALUES (random_uuid(), 'Bob', 200.0);
+INSERT INTO tb_worker (id, name, daily_Income) VALUES (random_uuid(), 'Maria', 300.0);
+INSERT INTO tb_worker (id, name, daily_Income) VALUES (random_uuid(), 'Alex', 250.0);
