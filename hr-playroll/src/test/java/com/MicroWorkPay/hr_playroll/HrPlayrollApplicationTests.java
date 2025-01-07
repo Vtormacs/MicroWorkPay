385f623c2c85b180341edcddd423a2ef1faa11a4
Vtormacs <macskids98play@hotmail.com>
2025-01-07 15:22:09 -0300

    feat: Projeto hr-playroll criado

---
+++ hr-playroll/src/test/java/com/MicroWorkPay/hr_playroll/HrPlayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MicroWorkPay.hr_playroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPlayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
