c329791ec57b286b8468e01829031fbc455d56e6
Vtormacs <macskids98play@hotmail.com>
2025-01-07 04:26:27 -0300

    feat(WorkerService): Implementação de funcionalidade de buscar todos worker

--- hr-worket/src/main/java/com/MicroWorkPay/hr_worket/services/WorkerService.java
+++ hr-worket/src/main/java/com/MicroWorkPay/hr_worket/services/WorkerService.java
@@ -1,8 +1,13 @@
 package com.MicroWorkPay.hr_worket.services;
 
+import com.MicroWorkPay.hr_worket.DTOs.WorkerInfoDTO;
+import com.MicroWorkPay.hr_worket.entities.Worker;
+import com.MicroWorkPay.hr_worket.mappers.WorkerMapper;
 import com.MicroWorkPay.hr_worket.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WorkerService {
@@ -10,5 +15,8 @@ public class WorkerService {
     @Autowired
     private WorkerRepository workerRepository;
 
-
+    public List<WorkerInfoDTO> findAll() {
+        List<Worker> workers = workerRepository.findAll();
+        return WorkerMapper.INSTANCE.workerListToWorkerInfoDTOList(workers);
+    }
 }
