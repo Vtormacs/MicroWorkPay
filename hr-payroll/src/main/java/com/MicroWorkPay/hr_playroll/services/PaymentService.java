9033dcd86b6f55dd54a77d18b503b481f984b3dd
Vtormacs <macskids98play@hotmail.com>
2025-01-08 14:48:32 -0300

    feat(PaymentService): chamada do projeto Worker mas conexao feita improvisada sera mudada, apenas para fim de testes

--- hr-payroll/src/main/java/com/MicroWorkPay/hr_playroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/MicroWorkPay/hr_playroll/services/PaymentService.java
@@ -1,14 +1,33 @@
 package com.MicroWorkPay.hr_playroll.services;
 
 import com.MicroWorkPay.hr_playroll.entities.Payment;
+import com.MicroWorkPay.hr_playroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(UUID workerId, int days){
-        return new Payment("Bob", 200.0, days);
+
+        Map<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("id", workerId.toString());
+
+        Worker worker = restTemplate.getForObject(workerHost + "/api/workers/{id}", Worker.class, uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
