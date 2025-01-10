9f257eec3f4b151b059f70118ecc1eb3b208a07e
Vtormacs <macskids98play@hotmail.com>
2025-01-10 14:49:05 -0300

    feat: Config basica do eureka no cliente PayRoll

--- hr-payroll/src/main/java/com/MicroWorkPay/hr_playroll/HrPlayrollApplication.java
+++ hr-payroll/src/main/java/com/MicroWorkPay/hr_playroll/HrPlayrollApplication.java
@@ -2,12 +2,13 @@ package com.MicroWorkPay.hr_playroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
 public class HrPlayrollApplication {
 
 	public static void main(String[] args) {
