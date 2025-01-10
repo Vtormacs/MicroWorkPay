fc906908ff36b5fd640825eb952422070518702f
Vtormacs <macskids98play@hotmail.com>
2025-01-10 14:46:01 -0300

    feat: Config basica do eureka no cliente Worker

--- hr-worket/src/main/java/com/MicroWorkPay/hr_worket/HrWorketApplication.java
+++ hr-worket/src/main/java/com/MicroWorkPay/hr_worket/HrWorketApplication.java
@@ -2,8 +2,10 @@ package com.MicroWorkPay.hr_worket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient	// Registra o serviço no Eureka
 public class HrWorketApplication {
 
 	public static void main(String[] args) {
