a436da95e85eebfe1420e86df6093ee79b378ebd
Vtormacs <macskids98play@hotmail.com>
2025-01-11 13:32:00 -0300

    feat: ADD Hystrix, configuracao basica mas ainda n tem regras para desvio de rota

--- hr-payroll/src/main/java/com/MicroWorkPay/hr_playroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/MicroWorkPay/hr_playroll/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package com.MicroWorkPay.hr_playroll.controllers;
 
 import com.MicroWorkPay.hr_playroll.entities.Payment;
 import com.MicroWorkPay.hr_playroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable UUID workerId, @PathVariable Integer days){
         try {
@@ -25,5 +27,9 @@ public class PaymentController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(UUID workerId, Integer days){
+        return ResponseEntity.ok(new Payment("Default", 0.0, days));
     }
 }
