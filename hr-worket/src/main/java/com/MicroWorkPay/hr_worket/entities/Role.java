b5c8067b534c9818a5238360b748a959c790e6b9
Vtormacs <macskids98play@hotmail.com>
2025-01-25 01:00:45 -0300

    feat: criação da entidade de Role

---
+++ hr-worket/src/main/java/com/MicroWorkPay/hr_worket/entities/Role.java
@@ -0,0 +1,24 @@
+package com.MicroWorkPay.hr_worket.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "tb_role")
+@Entity
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String roleName;
+
+}
