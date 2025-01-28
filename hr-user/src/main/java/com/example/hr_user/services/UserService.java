e27537c60dd1b166fc0ed2bb88ae4e01567b0b6b
Vtormacs <macskids98play@hotmail.com>
2025-01-28 12:14:47 -0300

    feat: criação do UserService e função de procurar com id um user

---
+++ hr-user/src/main/java/com/example/hr_user/services/UserService.java
@@ -0,0 +1,21 @@
+package com.example.hr_user.services;
+
+
+import com.example.hr_user.entities.User;
+import com.example.hr_user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(UUID id) {
+        User user = userRepository.findById(id).orElseThrow();
+        return user;
+    }
+}
