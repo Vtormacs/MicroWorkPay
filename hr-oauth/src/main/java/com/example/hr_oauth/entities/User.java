557c4fb1ff87eb582df1c5c870c65298c02f11e3
Vtormacs <macskids98play@hotmail.com>
2025-02-01 16:56:58 -0300

    feat: Atualizar entidade User e serviço UserService
    - Atualizada a entidade \User.java\ para implementar \UserDetails\ e adicionar métodos de autenticação.
    - Configurado o serviço \UserService.java\ para carregar usuários pelo email utilizando \UserFeignClient\.

--- hr-oauth/src/main/java/com/example/hr_oauth/entities/User.java
+++ hr-oauth/src/main/java/com/example/hr_oauth/entities/User.java
@@ -2,18 +2,20 @@ package com.example.hr_oauth.entities;
 
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class User implements Serializable {
+public class User implements UserDetails,Serializable {
     private static final long serialVersionUID = 1L;
 
     @EqualsAndHashCode.Include
@@ -22,4 +24,35 @@ public class User implements Serializable {
     private String email;
     private String password;
     private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
